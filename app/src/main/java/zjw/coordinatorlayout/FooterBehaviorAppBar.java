bdf126b27bf17161c22811be998f40ff19c3850e
jhonsonkilly <1245145380@qq.com>
2017-12-12 17:03:45 +0800

    完成根据手指和appbar所滑动的效果

---
+++ app/src/main/java/zjw/coordinatorlayout/FooterBehaviorAppBar.java
@@ -0,0 +1,28 @@
+package zjw.coordinatorlayout;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/9/26 0026.
+ */
+
+public class FooterBehaviorAppBar extends CoordinatorLayout.Behavior<View> {
+    public FooterBehaviorAppBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        float translationY = Math.abs(dependency.getY());
+        child.setTranslationY(translationY);
+        return true;
+    }
+}
---
+++ app/src/main/java/zjw/coordinatorlayout/FooterSmoothBehavior.java
@@ -0,0 +1,40 @@
+package zjw.coordinatorlayout;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+
+public class FooterSmoothBehavior extends CoordinatorLayout.Behavior<View> {
+    private int directionChange;
+
+    public FooterSmoothBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
+        if (dy > 0 && directionChange < 0 || dy < 0 && directionChange > 0) {
+            child.animate().cancel();
+            directionChange = 0;
+        }
+        directionChange += dy;
+        if (directionChange > child.getHeight() && child.getVisibility() == View.VISIBLE) {
+            child.setVisibility(View.GONE);
+        } else if (directionChange < 0 && child.getVisibility() == View.GONE) {
+            child.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+}
